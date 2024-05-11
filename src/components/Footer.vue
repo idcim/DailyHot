025e95db61e8544fe56c2df9f4e6f420547b056e
Smart Site <idcim@qq.com>
2024-05-12 00:36:44 +0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,7 @@
 <template>
   <footer>
     <div class="copyright">
-      <n-text class="description" v-html="packageJson.description" />
-      <n-text
-        class="author"
-        :depth="3"
-        v-html="packageJson.author"
-        @click="jumpLink(packageJson.github)"
-      />
+      
     </div>
     <n-text
       v-if="icp"
